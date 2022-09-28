0e87b20b4caabcf7a9a42a8f67a7799193a56470
pradyuman shukla <shuklapradyuman786@gmail.com>
2022-09-29 02:46:06 +0530

    day2

---
+++ mailtrap/src/Components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import  { Component } from "react";
+import { Button } from "./Button";
+import { MenuItems } from "./MenuItems";
+import "./Navbar.css"
+
+
+class Navbar extends Component {
+    state ={clicked:false}
+
+    handleClick =()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render(){
+        return (
+            <nav className="NavbarItems">
+              <h1 className="navbar-logo" > <img  src="https://mailtrap.io/wp-content/uploads/2021/04/mailtrap-new-logo.svg" alt="mailtrap" />   </h1>
+              <div className="menu-icon" onClick={this.handleClick}><i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i></div>
+              <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                {MenuItems.map((item,index)=>{
+                    return (
+                        <li key={index}>
+                            <a className={item.name} href={item.Url}> {item.title}</a>
+                        </li>
+                    )
+                })}
+              </ul>
+                <Button >Sign up</Button>
+                <Button >Log In</Button>
+
+            </nav>
+        )
+    }
+}
+export default Navbar
---
+++ mailtrap/src/Components/Navbar/NavbarUpper.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+// import { Box, Text } from '@chakra-ui/react'
+
+// function NavbarUpper() {
+//   return (
+//     <div>
+// <Box bg='#1A2E44' w='100%' p={20} color='#ffffff' fontSize="18px" lineHeight="31px" fontWeight="600">
+//   Stand with Ukraine   <Text as='b' fontSize="12px" >UA</Text>  <Text as='u' fontSize="18px" >Donate to support</Text> 
+// </Box>
+
+
+//     </div>
+//   )
+// }
+
+// export default NavbarUpper
