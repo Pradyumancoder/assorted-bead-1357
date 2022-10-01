f455e18a4b122c0c6b1b48d301874f96d634c041
pradyuman shukla <shuklapradyuman786@gmail.com>
2022-10-01 21:46:48 +0530

    day4

--- mailtrap/src/App.js
+++ mailtrap/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Footer from './Components/Footer/Footer';
-// import Navbar from "./Components/Navbar/Navbar"
+// import Footer from './Components/Footer/Footer';
+import Home from './Components/Homepage/Home';
+import Navbar from "./Components/Navbar/Navbar"
+import NavbarUpper from "./Components/Navbar/NavbarUpper"
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
-      <Footer/>
+      <NavbarUpper />
+      <Navbar />
+    
+      {/* <Footer/> */}
+      <Home/>
     </div>
   );
 }
